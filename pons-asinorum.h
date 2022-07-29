85f1d5389b4f82c2ab259fc40868795d869a17bc
nathandxn <nathandxn@gmail.com>
2022-07-29 16:21:14 -0500

    adding absolute value function

--- pons-asinorum.h
+++ pons-asinorum.h
@@ -9,6 +9,18 @@ namespace ponsas {
 
 const double PI = 3.141592653589793;
 
+template <typename T>
+T abs(T x);
+
+// function implementations
+
+// templated absolute value function or |x|
+template <typename T>
+T abs(T x) {
+    if (x < 0) return -x;
+    return x;
+}
+
 }
 
 #endif // PONSASINORUM_H
