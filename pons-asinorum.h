22524aabdab52259085d9d73d3cf1c8bc7e4d4d9
nathandxn <nathandxn@gmail.com>
2022-07-29 16:25:33 -0500

    adding factorial function

--- pons-asinorum.h
+++ pons-asinorum.h
@@ -11,6 +11,7 @@ const double PI = 3.141592653589793;
 
 template <typename T>
 T abs(T x);
+int factorial(int n);
 
 // function implementations
 
@@ -19,6 +20,12 @@ template <typename T>
 T abs(T x) {
     if (x < 0) return -x;
     return x;
+}
+
+// factorial function
+int factorial(int n) {
+    if (n < 0) throw std::invalid_argument("std::invalid_argument: ponsas::factorial only takes non-negative integer values.")
+    return n == 0 ? 1 : n * factorial(n - 1);
 }
 
 }
